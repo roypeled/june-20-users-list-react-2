b9508830b493eb51f0e3214c1ea717b1c0f21564
Roy Peled <peled.roy@gmail.com>
2020-11-08 18:24:39 +0200

    use effect

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
-import React from 'react';
-import './App.scss';
+import React, { useState } from 'react';
 import { UsersList } from './users-lists/UsersList';
 import { ToggleButton } from './ToggleButton';
+import './App.scss';
 
-export class App extends React.Component {
+export function App(props) {
 
-  constructor() {
-    super();
-    
-    this.state = {
-      isLoggedIn: false,
-    }
-  }
+  let [ isLoggedIn, setIsLoggedIn ] = useState(false);
 
-  login() {
-    this.setState({
-      isLoggedIn: true
-    })
-  }
-
-  render() {
-    if(!this.state.isLoggedIn) {
-      return <div className="App">
-        Please login!
-        <br/>
-        <button onClick={() => this.login()}>Login</button>
-
-        <ToggleButton showText="Help!" hideText="Close">
-          <p>Explain how to login</p>
-        </ToggleButton>
-      </div>
-    }
-
+  if(!isLoggedIn) {
     return <div className="App">
-      <UsersList></UsersList>
+      Please login!
+      <br/>
+      <button onClick={() => setIsLoggedIn(true) }>Login</button>
     </div>
   }
+
+  return <div className="App">
+    <UsersList></UsersList>
+  </div>
 }
+
--- src/users-lists/UsersList.js
+++ src/users-lists/UsersList.js
@@ -1,51 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './UsersList.scss';
 import { User } from './user/User';
 
-export class UsersList extends React.Component {
+async function fetchUsers() {
+    const result = await fetch('https://jsonplaceholder.typicode.com/users/');
+    return result.json();
+}
 
-    constructor() {
-        super();
+export function UsersList(props) {
 
-        this.state = {
-            users: [],
-        };
+    const [users, setUsers] = useState([]);
 
-        this.init();
+    useEffect(async () => {
+        const result = await fetchUsers();
+        setUsers(result);
+    }, []);
+
+    if (users.length <= 0) {
+        return <div>Loading Users...</div>
     }
 
-    async init() {
-        const users = await this.fetchUsers();
-        this.setState({ users: users });
+    return <div className="users-list">
+        <h1>Users list</h1>
 
-        setTimeout(() => this.init(), 5000);
-    }
-
-    async fetchUsers() {
-        const result = await fetch('https://jsonplaceholder.typicode.com/users/');
-        return result.json();
-    }
-
-    render() {
-        if (this.state.users.length <= 0) {
-            return <div>Loading Users...</div>
-        }
-
-        return <div className="users-list">
-            <h1>Users list</h1>
-
-            <div className="list">
-                <div>
-                    { 
-                        this.state.users.map(user => <User 
-                            name={ user.name } 
-                            email={ user.email }
-                            address={ user.address }
-                            companyName={ user.company.name }></User>)
-                    }
-                </div>
-            </div>
-        </div>;
-    }
+        <div className="list">
+            { 
+                users.map(user => <User 
+                    name={ user.name } 
+                    email={ user.email }
+                    address={ user.address }
+                    companyName={ user.company.name }></User>)
+            }
+        </div>
+    </div>;
 }
 
